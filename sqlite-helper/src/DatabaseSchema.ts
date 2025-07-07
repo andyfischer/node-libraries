b9cbd003eec3366d0993b3640f0b1b7ecf81d526
Andrew Fischer <andy.fischer@gmail.com>
2025-07-07 14:32:44 -0600

    sqlite-wrapper 0.1.0

--- sqlite-helper/src/DatabaseSchema.ts
+++
@@ -1,5 +0,0 @@
-export interface DatabaseSchema {
-    name: string
-    statements: string[]
-    initialData?: string[]
-}
--- sqlite-helper/src/index.ts
+++
@@ -1,3 +0,0 @@
-
-export { SqliteDatabase } from './SqliteDatabase'
-export { DatabaseLoader } from './DatabaseLoader'
